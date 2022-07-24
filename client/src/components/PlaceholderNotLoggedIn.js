a46f9643d3b6d0e89d3a05ba761d9504a00cc426
antoninaorobets <pani.antonina.don@gmail.com>
2022-07-24 17:38:20 -0400

    add Log in button on Please log in placeholder

--- client/src/components/PlaceholderNotLoggedIn.js
+++ client/src/components/PlaceholderNotLoggedIn.js
@@ -1,18 +1,32 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Button, Container } from '@mui/material'
+import { NavLink } from 'react-router-dom'
 
 function Placeholder() {
   return (
     <Box
+    align="center" 
       sx={{
         bgcolor: 'background.paper',
         pt: 8,
         pb: 6,
       }}
     >
-      <Typography align="center" component="h1"  variant="h4" color="text.secondary" paragraph sx={{ marginBottom: 4 }}>
-        Please Log in
+ 
+      <Typography align="center" component="h6"  variant="h5" color="text.secondary" paragraph sx={{ marginBottom: 4 }}>
+        Please Log in 
       </Typography>
+      <Box component="span" sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <NavLink to="/login" style={{ textDecoration: 'none' }}>
+        <Button direction="row"
+            variant="outlined"
+            size="medium"
+            sx={{ pl: 8, pr: 8 }}
+            >
+            Login
+        </Button>
+     </NavLink>
+    </Box>
     </Box>
   )
 }
